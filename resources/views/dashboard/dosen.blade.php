5a9c27c44ca7ed0d1ede67be062d55d66b6c7e50
Kievland <157392165+Kievland@users.noreply.github.com>
2026-01-05 13:28:09 +0700

    Menambahkan 4 halaman dashboard untuk Dosen, Operator, Wadek, dan Dekan

---
+++ resources/views/dashboard/dosen.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Dashboard Dosen</h1>
+        <p>Selamat datang, Bapak/Ibu Dosen. Silakan ajukan izin di sini.</p>
+        </div>
+@endsection
